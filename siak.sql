00c24bfe37e3aefda87548b9f7de6b52639ae0da
islahatun <islahatunnufusi98@gmailcom>
2021-08-05 15:56:12 +0700

    selesai

--- siak.sql
+++ siak.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2021 at 11:23 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.3.21
+-- Generation Time: Aug 05, 2021 at 10:54 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.3.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,26 +40,16 @@ CREATE TABLE `acc_surat` (
 --
 
 INSERT INTO `acc_surat` (`id_acc`, `nik`, `surat`, `tanggal`, `status`) VALUES
-(1, '1234567890111213', 'ktp', '2018-06-21', ''),
-(2, '1234567890111213', 'ktp', '2018-06-21', ''),
-(3, '1234567890111213', 'usaha', '2018-06-21', ''),
-(4, '1234567890111213', 'domisili', '2018-06-21', ''),
-(5, '1234567890111213', 'domisili', '2018-06-21', ''),
-(6, '1234567890111213', 'domisili', '2018-06-21', ''),
-(7, '1234567890111213', 'rame', '2019-06-21', ''),
-(8, '1234567890111213', 'usaha', '2019-06-21', ''),
-(11, '1234567890111213', 'usaha', '2021-06-19', ''),
-(12, '1234567890111213', 'nikah', '2021-06-19', ''),
-(13, '1234567890111213', 'pindah', '2021-06-19', ''),
-(14, '3601204107740005', 'ktp', '2021-06-26', ''),
-(15, '3601204107740005', 'usaha', '2021-06-27', ''),
-(16, '3601204107740005', 'pindah', '2021-06-27', ''),
-(17, '3601204107740005', 'domisili', '2021-06-27', ''),
-(18, '3601204107740005', 'rame', '2021-06-27', ''),
-(19, '3601204107740005', 'rame', '2021-06-27', ''),
-(20, '3601204107740005', 'nikah', '2021-07-03', ''),
-(21, '3601204107740005', 'nikah', '2021-07-03', ''),
-(22, '3601204107740008', 'sktm', '2021-07-11', '');
+(2, '3601204107740005', 'ktp', '2021-07-30', ''),
+(3, '3601204107740005', 'nikah', '2021-07-30', ''),
+(4, '3601204107740005', 'nikah', '2021-07-31', ''),
+(5, '3601204107740005', 'usaha', '2021-07-30', ''),
+(6, '3601204107740005', 'sktm', '2021-07-30', ''),
+(7, '3601204107740005', 'sktm', '2021-07-30', ''),
+(8, '3601204107740005', 'nikah', '2021-08-02', ''),
+(9, '3601204107740005', 'nikah', '2021-08-02', ''),
+(10, '3601204107740005', 'ktp', '2021-08-05', ''),
+(11, '3601204107740005', 'domisili', '2021-08-05', '');
 
 -- --------------------------------------------------------
 
@@ -124,8 +114,8 @@ CREATE TABLE `artikel` (
 --
 
 INSERT INTO `artikel` (`id`, `judul`, `isi`, `foto`) VALUES
-(1, 'Tentang Desa', '<p>desa ini bla bla bla bla bla bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla</p>', 'Salinan_IMG-20190715-WA0005.jpg'),
-(2, 'Kegitan Sosialisasi Usaha Kecil Menengah', '<p>Salah satunya adalah pelatihan kewirausahaan yang diselenggarakan bg UMKM di desa Banjar. Acara pelatihan dimulai dengan Registrasi oleh peserta pada pukul 08.00 WIB. Peserta mengisi daftar hadir, penerimaan modul, ATK, dan materi. Adapun pPelaksanaan kegiatan pelatihan kewirausahaan bagi UMKM di wilayah Desa Banjar diawali dengan acara pembukaan. Pelatihan dibuka oleh Kepala Dinas Koperasi UKM dan ditemani oleh  Kepala Desa.Para peserta diseleksi melalui koordinasi antara padukuhan, desa,serta Dinas Koperasi Usaha Kecil dan Menengah  berdasarkan kriteria yang telah disusun dan disosialisasikan kepada masyarakat melalui rapat koordinasi dengan perangkat desa.</p>', 'WhatsApp_Image_2021-04-15_at_15_30_111.jpeg');
+(2, 'Kegitan Sosialisasi Usaha Kecil Menengah', '<p>Salah satunya adalah pelatihan kewirausahaan yang diselenggarakan bg UMKM di desa Banjar. Acara pelatihan dimulai dengan Registrasi oleh peserta pada pukul 08.00 WIB. Peserta mengisi daftar hadir, penerimaan modul, ATK, dan materi. Adapun pPelaksanaan kegiatan pelatihan kewirausahaan bagi UMKM di wilayah Desa Banjar diawali dengan acara pembukaan. Pelatihan dibuka oleh Kepala Dinas Koperasi UKM dan ditemani oleh  Kepala Desa.Para peserta diseleksi melalui koordinasi antara padukuhan, desa,serta Dinas Koperasi Usaha Kecil dan Menengah  berdasarkan kriteria yang telah disusun dan disosialisasikan kepada masyarakat melalui rapat koordinasi dengan perangkat desa.</p>', 'WhatsApp_Image_2021-04-15_at_15_30_111.jpeg'),
+(5, 'Kapolsek Banjar Hadiri “Panen Cabai Merah” Bersama Bupati Pandeglang Di Desa Banjar', '<p>Untuk mengawal kegiatan pemerintah tentang produksi tanaman sayuran yang bersumber dari dana APBN tahun 2017 Kapolsek Banjar AKP Reni Widowati hadiri panen cabai merah bersama Bupati Pandeglang Hj.Irna narulita di Desa Sukamanah kec.Kaduhejo kab.Pandeglang.Rabu 29/11. Dalam kesempatan tersebut Bupati Irna mengatakan bahwa cabai merah merupakan salah satu komoditas tanaman holtikultura yang sangat dibutuhkan dan merupakan kebutuhan pokok masyarakat banyak.&nbsp;Kapolsek Banjar bersama Bupati Pandeglang berkesempatan untuk memetik Cabai merah, kemudian acara dilanjutkan dengan Acara di lanjutkan dengan Penyerahan bantuan bibit tanaman Hortikultura untuk kelompok tani dalam program One Village One Product&nbsp;berupa bibit rambutan, bibit durian serta bibit cabai, untuk dibagikan dan ditanam dipekarangan rumah warga.</p>\r\n\r\n<p>&nbsp;</p>', 'foto_1.jpg');
 
 -- --------------------------------------------------------
 
@@ -185,17 +175,17 @@ INSERT INTO `kampung` (`id`, `kampung`) VALUES
 CREATE TABLE `ktp` (
   `id` int(11) NOT NULL,
   `nik` varchar(16) NOT NULL,
-  `surat` varchar(30) NOT NULL
+  `surat` varchar(30) NOT NULL,
+  `tanggal_surat` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `ktp`
 --
 
-INSERT INTO `ktp` (`id`, `nik`, `surat`) VALUES
-(2, '1234567890111213', 'ktp'),
-(3, '1234567890111213', 'ktp'),
-(4, '3601204107740005', 'ktp');
+INSERT INTO `ktp` (`id`, `nik`, `surat`, `tanggal_surat`) VALUES
+(2, '3601204107740005', 'ktp', '2021-07-30'),
+(3, '3601204107740005', 'ktp', '2021-08-05');
 
 -- --------------------------------------------------------
 
@@ -217,7 +207,8 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`nik`, `nama`, `sandi`, `level`, `aktif`) VALUES
 ('3601204107740005', 'TITIN', '$2y$10$O.f4X.lKfOuSDBYFLD2mhewhbvLRsg/mAKP296fDIdSZ.WlqIdIka', 'masyarakat', '1'),
-('123', 'SISKA', '$2y$10$Hj1cC2G9bD/osGC4kCE/H.OZKAe9yq17E1i3tFnEFrNdOUTButWu2', 'admin', '1');
+('123', 'SISKA', '$2y$10$Hj1cC2G9bD/osGC4kCE/H.OZKAe9yq17E1i3tFnEFrNdOUTButWu2', 'admin', '1'),
+('3601204107740037', 'RENDI', '$2y$10$eJnTiaWxPX/gJyZ9qIIkjeAUt12kKOekl.fZvPZxxv3AVdDS/8xti', 'masyarakat', '1');
 
 -- --------------------------------------------------------
 
@@ -232,7 +223,7 @@ CREATE TABLE `masyarakat` (
   `nama` varchar(50) NOT NULL,
   `tempat_lahir` varchar(50) NOT NULL,
   `tanggal_lahir` date NOT NULL,
-  `jk` enum('Laki-Laki','Perempuan','','') NOT NULL,
+  `jk` varchar(15) NOT NULL,
   `agama` varchar(15) NOT NULL,
   `alamat` varchar(30) NOT NULL,
   `rt/rw` varchar(10) NOT NULL,
@@ -241,7 +232,7 @@ CREATE TABLE `masyarakat` (
   `kabupaten` varchar(30) NOT NULL,
   `pendidikan_terakhir` varchar(10) NOT NULL,
   `pekerjaan` varchar(50) NOT NULL,
-  `status` enum('Belum Menikah','Menikah','','') NOT NULL,
+  `status` varchar(30) NOT NULL,
   `kewarganegaraan` varchar(30) NOT NULL,
   `gol` varchar(3) NOT NULL,
   `gambar` varchar(255) NOT NULL
@@ -252,9 +243,9 @@ CREATE TABLE `masyarakat` (
 --
 
 INSERT INTO `masyarakat` (`id`, `kk`, `nik`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jk`, `agama`, `alamat`, `rt/rw`, `desa`, `kecamatan`, `kabupaten`, `pendidikan_terakhir`, `pekerjaan`, `status`, `kewarganegaraan`, `gol`, `gambar`) VALUES
-(3, '3601200801080513', '3601204107740005', 'TITIN', 'Pandeglang', '1974-01-07', 'Perempuan', 'Islam', 'KP KUKUPU', '005/001', 'BANJAR', 'BANJAR', 'PANDEGLANG', 'SMK', 'IBU RUMAH TANGGA', 'Menikah', 'indonesia', 'B', 'default.jpg'),
-(5, '3601200801080513', '3601204107740008', 'Ummi athiyah', 'Serang', '1998-02-04', 'Perempuan', 'Islam', 'serang', '005/001', 'serang', 'serang', 'Serang', 'SMK', 'Mahasiswa', 'Belum Menikah', 'Indonesia', 'O', 'default.jpg'),
-(7, '3601200801080513', '3601204107740008', 'Ummi athiyah', 'Serang', '1998-02-04', 'Perempuan', 'Islam', 'serang', '005/001', 'serang', 'serang', 'Serang', 'SMK', 'Mahasiswa', 'Belum Menikah', 'Indonesia', 'B', 'default.jpg');
+(3, '3601200801080513', '3601204107740005', 'TITIN', 'Pandeglang', '1974-01-07', 'LAKI LAKI', 'ISLAM', 'KP KUKUPU KUPU', '005/0021', 'BANJAR', 'BANJAR', 'PANDEGLANG', 'SMk', 'IBU RUMAH TANGGA', 'BELUM MENIKAH', 'INDONESIA', 'O', 'download.jpg'),
+(8, '3601200801080541', '3601205107000002', 'AYUNI', 'PANDEGLANG', '2000-07-11', 'Perempuan', 'ISLAM', 'KP. KUKUPU', '005', '001', 'BANJAR', 'PANDEGLANG', 'SMA', 'PELAJAR/MAHASISWA', 'Belum Menikah', 'INDONESIA', 'B', 'default.jpg'),
+(9, '3601200801080556', '3601204107740037', 'RENDI', 'PANDEGLANG', '1998-08-06', 'LAKI LAKI', 'ISLAM', 'KP. BANJAR', '002/001', 'BANJAR', 'BANJAR', 'PANDEGLANG', 'SMA', 'PELAJAR/MAHASISWA', 'BELUM MENIKAH', 'INDONESIA', 'O', 'default.jpg');
 
 -- --------------------------------------------------------
 
@@ -292,21 +283,6 @@ INSERT INTO `menu` (`id`, `nama`, `url`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pengguna`
---
-
-CREATE TABLE `pengguna` (
-  `id` int(11) NOT NULL,
-  `nik` varchar(16) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `password` varchar(225) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  `is_active` char(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `struktur-organisasi`
 --
 
@@ -322,15 +298,17 @@ CREATE TABLE `struktur-organisasi` (
   `kaur_keuangan` varchar(50) NOT NULL,
   `kaur_umum` varchar(50) NOT NULL,
   `kadus1` varchar(50) NOT NULL,
-  `kadus2` varchar(50) NOT NULL
+  `kadus2` varchar(50) NOT NULL,
+  `visi` text NOT NULL,
+  `misi` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `struktur-organisasi`
 --
 
-INSERT INTO `struktur-organisasi` (`id`, `lurah`, `sekretaris`, `bendahara`, `kasi_pelayanan`, `kasi_pemerintahan`, `kasi_pemberdayaan`, `kaur_perencanaan`, `kaur_keuangan`, `kaur_umum`, `kadus1`, `kadus2`) VALUES
-(1, 'umam', 'ummu kulsum', 'Rina', 'lulu', 'Imam', 'Candra', 'Sisi', 'Intan', 'Elia', 'Siska', 'Dwi Cahya');
+INSERT INTO `struktur-organisasi` (`id`, `lurah`, `sekretaris`, `bendahara`, `kasi_pelayanan`, `kasi_pemerintahan`, `kasi_pemberdayaan`, `kaur_perencanaan`, `kaur_keuangan`, `kaur_umum`, `kadus1`, `kadus2`, `visi`, `misi`) VALUES
+(1, 'umam', 'Santi Nurafrida', 'Rina Nuraini', 'Andri Putra', 'Imam', 'Candra', 'Sisi', 'Intan', 'Rima', 'Siska', 'Dwi Cahya', '<ol>\r\n	<li>pelayanan yang bagusmmmmmmmm</li>\r\n	<li>mmmmmmmmmmmmm</li>\r\n	<li>mmmmmmmmmmmmmmmmmmmmmmm</li>\r\n	<li>mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm mmmmmmmmmmmmmmmmm</li>\r\n</ol>', '<ol>\r\n	<li>melayani masyarakatg gggg</li>\r\n	<li>ggggggggggg ghrth dferg dfgffdgfxcvrtdcv xfertbaHAT G HAguihaOHkg67qw5</li>\r\n</ol>');
 
 -- --------------------------------------------------------
 
@@ -365,15 +343,16 @@ CREATE TABLE `surat_domisili` (
   `id` int(11) NOT NULL,
   `nik` varchar(16) NOT NULL,
   `surat` varchar(16) NOT NULL,
-  `alamat_domisili` varchar(50) NOT NULL
+  `alamat_domisili` varchar(50) NOT NULL,
+  `tanggal_surat` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `surat_domisili`
 --
 
-INSERT INTO `surat_domisili` (`id`, `nik`, `surat`, `alamat_domisili`) VALUES
-(1, '1234567890111213', 'domisili', '');
+INSERT INTO `surat_domisili` (`id`, `nik`, `surat`, `alamat_domisili`, `tanggal_surat`) VALUES
+(9, '3601204107740005', 'domisili', 'Serang', '2021-08-05');
 
 -- --------------------------------------------------------
 
@@ -389,15 +368,9 @@ CREATE TABLE `surat_izin_rame` (
   `hiburan` varchar(100) NOT NULL,
   `tanggal_pelaksanaan` date NOT NULL,
   `waktu` time NOT NULL,
-  `tempat` varchar(50) NOT NULL
+  `tempat` varchar(50) NOT NULL,
+  `tanggal_surat` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `surat_izin_rame`
---
-
-INSERT INTO `surat_izin_rame` (`id`, `nik`, `surat`, `maksud`, `hiburan`, `tanggal_pelaksanaan`, `waktu`, `tempat`) VALUES
-(1, '3601204107740005', 'rame', 'Pernikahan', 'Orgen', '2021-06-27', '12:18:00', 'Maja');
 
 -- --------------------------------------------------------
 
@@ -409,15 +382,20 @@ CREATE TABLE `surat_nikah` (
   `id` int(11) NOT NULL,
   `nik` varchar(16) NOT NULL,
   `surat` varchar(30) NOT NULL,
-  `pasangan` varchar(30) NOT NULL
+  `pasangan` varchar(30) NOT NULL,
+  `alamat_pasangan` varchar(200) NOT NULL,
+  `tanggal_surat` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `surat_nikah`
 --
 
-INSERT INTO `surat_nikah` (`id`, `nik`, `surat`, `pasangan`) VALUES
-(1, '3601204107740005', 'nikah', 'Tatan');
+INSERT INTO `surat_nikah` (`id`, `nik`, `surat`, `pasangan`, `alamat_pasangan`, `tanggal_surat`) VALUES
+(1, '3601204107740005', 'nikah', 'Ardi', 'Kampung', '2021-07-30'),
+(2, '3601204107740005', 'nikah', 'TATAN', 'Kota', '2021-07-31'),
+(3, '3601204107740005', 'nikah', 'TATAN', 'kota', '2021-08-02'),
+(4, '3601204107740005', 'nikah', 'Roni', 'Kabupaten', '2021-08-02');
 
 -- --------------------------------------------------------
 
@@ -435,16 +413,9 @@ CREATE TABLE `surat_pindah` (
   `kecamatan_pindah` varchar(30) NOT NULL,
   `kabupaten/kota_pindah` varchar(30) NOT NULL,
   `provinsi_pindah` varchar(30) NOT NULL,
-  `alasan_pindah` varchar(100) NOT NULL
+  `alasan_pindah` varchar(100) NOT NULL,
+  `tanggal_surat` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `surat_pindah`
---
-
-INSERT INTO `surat_pindah` (`id`, `nik`, `surat`, `alamat_pindah`, `rt/rw_pindah`, `desa_pindah`, `kecamatan_pindah`, `kabupaten/kota_pindah`, `provinsi_pindah`, `alasan_pindah`) VALUES
-(1, '1234567890111213', 'pindah', 'Kalapa Lima', '001', 'Sukamanah', 'Baros', 'Serang', 'Banten', 'Sekolah'),
-(2, '3601204107740005', 'pindah', 'Kalapa Lima', '001', 'Sukamanah', 'Baros', 'Serang', 'Banten', 'Sekolah');
 
 -- --------------------------------------------------------
 
@@ -456,15 +427,17 @@ CREATE TABLE `surat_sktm` (
   `id` int(11) NOT NULL,
   `nik` varchar(16) NOT NULL,
   `surat` varchar(20) NOT NULL,
-  `alasan` varchar(225) NOT NULL
+  `alasan` varchar(225) NOT NULL,
+  `tanggal_surat` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `surat_sktm`
 --
 
-INSERT INTO `surat_sktm` (`id`, `nik`, `surat`, `alasan`) VALUES
-(1, '3601204107740008', 'sktm', 'Keperluan Kampus');
+INSERT INTO `surat_sktm` (`id`, `nik`, `surat`, `alasan`, `tanggal_surat`) VALUES
+(1, '3601204107740005', 'sktm', 'Dana Bantuan', '2021-07-30'),
+(2, '3601204107740005', 'sktm', 'Baksos', '2021-07-30');
 
 -- --------------------------------------------------------
 
@@ -477,21 +450,16 @@ CREATE TABLE `surat_usaha` (
   `nik` varchar(16) NOT NULL,
   `surat` varchar(30) NOT NULL,
   `jenis_usaha` varchar(50) NOT NULL,
-  `tempat_usaha` varchar(50) NOT NULL
+  `tempat_usaha` varchar(50) NOT NULL,
+  `tanggal_surat` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `surat_usaha`
 --
 
-INSERT INTO `surat_usaha` (`id`, `nik`, `surat`, `jenis_usaha`, `tempat_usaha`) VALUES
-(1, '1234567890111213', 'usaha', 'Agen Sembako', 'Serang'),
-(2, '1234567890111213', 'usaha', 'jualan kramik', 'baros'),
-(3, '1234567890111213', 'usaha', 'jualan kramik', 'baros'),
-(4, '1234567890111213', 'usaha', 'jualan baju', 'baros'),
-(5, '1234567890111213', 'usaha', 'jualan baju', 'baros'),
-(6, '1234567890111213', 'usaha', 'jualan baju', 'baros'),
-(7, '3601204107740005', 'usaha', 'Agen Sembako', 'Serang');
+INSERT INTO `surat_usaha` (`id`, `nik`, `surat`, `jenis_usaha`, `tempat_usaha`, `tanggal_surat`) VALUES
+(1, '3601204107740005', 'usaha', 'Toko Agen', 'Desa', '2021-07-30');
 
 --
 -- Indexes for dumped tables
@@ -537,12 +505,6 @@ ALTER TABLE `masyarakat`
 -- Indexes for table `menu`
 --
 ALTER TABLE `menu`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `pengguna`
---
-ALTER TABLE `pengguna`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -601,7 +563,7 @@ ALTER TABLE `surat_usaha`
 -- AUTO_INCREMENT for table `acc_surat`
 --
 ALTER TABLE `acc_surat`
-  MODIFY `id_acc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id_acc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `akses_menu`
@@ -613,7 +575,7 @@ ALTER TABLE `akses_menu`
 -- AUTO_INCREMENT for table `artikel`
 --
 ALTER TABLE `artikel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `kampung`
@@ -625,25 +587,19 @@ ALTER TABLE `kampung`
 -- AUTO_INCREMENT for table `ktp`
 --
 ALTER TABLE `ktp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `masyarakat`
 --
 ALTER TABLE `masyarakat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
---
--- AUTO_INCREMENT for table `pengguna`
---
-ALTER TABLE `pengguna`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `struktur-organisasi`
@@ -661,37 +617,37 @@ ALTER TABLE `surat`
 -- AUTO_INCREMENT for table `surat_domisili`
 --
 ALTER TABLE `surat_domisili`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `surat_izin_rame`
 --
 ALTER TABLE `surat_izin_rame`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `surat_nikah`
 --
 ALTER TABLE `surat_nikah`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `surat_pindah`
 --
 ALTER TABLE `surat_pindah`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `surat_sktm`
 --
 ALTER TABLE `surat_sktm`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `surat_usaha`
 --
 ALTER TABLE `surat_usaha`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
