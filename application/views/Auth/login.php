9b39ff1f8b0be389969026db64516c56fb1a65b0
islahatun <islahatunnufusi98@gmailcom>
2021-08-16 11:30:47 +0700

    perbaikan tampilan

--- application/views/Auth/login.php
+++ application/views/Auth/login.php
@@ -1,15 +1,16 @@
  <!-- Outer Row -->
  <div class="row justify-content-center">
 
-     <div class="col-5">
-
+     <div class="col-xl-10 col-lg-12 col-md-9">
          <div class="card o-hidden border-0 shadow-lg my-5">
              <div class="card-body p-0">
                  <!-- Nested Row within Card Body -->
                  <div class="row">
-
-                     <div class="col-lg">
-                         <div class="p-5">
+                     <div class="col-lg-6 d-none d-lg-block">
+                         <img class="d-block w-100 h-100" src="<?= base_url('assets/img/poto1.png') ?>" alt="First slide">
+                     </div>
+                     <div class="col-lg-6">
+                         <div class="p-4">
                              <div class="text-center">
                                  <h1 class="h4 text-gray-900 mb-4">Login</h1>
                              </div>
@@ -36,8 +37,8 @@
                  </div>
              </div>
          </div>
-
      </div>
+
 
  </div>
 
